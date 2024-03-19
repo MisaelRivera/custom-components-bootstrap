d0cd10bf519b5046d30c502bb58ecc00dd3f3610
Misael Rivera <misaelrivera121193@gmail.com>
2024-03-19 17:40:41 -0600

    First commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+  import HelloWorld from './components/HelloWorld.vue';
+  import CustomInput from './components/CustomInput.vue';
+</script>
+
+<template>
+  <HelloWorld msg="Vite + Vue" />
+  <div class="row">
+    <CustomInput 
+      text="Name"
+      size="col-md-5"
+    />
+  </div>
+</template>
---
+++ src/components/CustomInput.vue
@@ -0,0 +1,42 @@
+<script setup>
+   const props =  defineProps({
+        text: {
+            type: String
+        },
+
+        disabled: {
+            type: Boolean,
+            default: false
+        },
+
+        type: {
+            type: String,
+            default: 'text',
+        },
+
+        id: String,
+
+        size: String,
+
+        classes: {
+            type: Array,
+            default: () => []
+        }
+    });
+</script>
+
+<template>
+    <div :class="[...classes, size, 'mb-3']">
+        <label 
+            :for="id" 
+            class="form-label">
+            {{ text }}
+        </label>
+        <input 
+            :type="type" 
+            class="form-control"
+            :id="id" 
+            :placeholder="text"
+            :disabled="disabled">
+    </div>
+</template>
